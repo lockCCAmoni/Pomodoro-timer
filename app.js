0faeefefd30e491be78529408c6cca3bdf88fede
AnimLi <leowork430@gmail.com>
2026-01-15 23:33:02 +0800

    Add an Audio

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 const timer = document.getElementById('timer');
+const audio = new Audio("https://dl.dropboxusercontent.com/s/1cdwpm3gca9mlo0/kick.mp3");
 
 let timeLeft = 1500; // 25 minutes in seconds
 let interval; // Variable to store the interval ID
 
 const updateTimer = () => {
-    const minutes = Math.floor(timeLeft / 60);
+    const minutes = Math.floor(timeLeft / 60); //
     const seconds = timeLeft % 60;
 
     timer.innerHTML = `${minutes.toString().padStart(2, "0")}
@@ -23,6 +24,13 @@ const startTimer = () => {
 
         if(timeLeft === 0) {
             clearInterval(interval);
+            
+            audio.play().then(() => {
+                // Audio played successfully
+                audio.pause();
+                audio.currentTime = 0;
+            });
+
             alert("Time's up!");
             timeLeft = 1500; // Reset to 25 minutes
             updateTimer();
